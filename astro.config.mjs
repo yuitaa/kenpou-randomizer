d3f587b945ece28864dbbacb4c246b2166c93f49
yuitaa <95457974+yuitaa@users.noreply.github.com>
2026-03-05 11:21:42 +0900

    site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://kenpou-randomizer.vercel.app/',
   vite: {
     plugins: [tailwindcss()],
   },
